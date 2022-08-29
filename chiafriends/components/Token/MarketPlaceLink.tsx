b7bf8eb83aa25ae60da053938f6822973319b00b
AlexClayy <96576445+AlexClayy@users.noreply.github.com>
2022-08-29 15:09:24 +0700

    Update MarketPlaceLink.tsx

--- chiafriends/components/Token/MarketPlaceLink.tsx
+++ chiafriends/components/Token/MarketPlaceLink.tsx
@@ -5,7 +5,6 @@ import { OpenSeaIcon } from "../Icons";
 const MarketPlaceLinks = ({ id }: { id: string }) => {
 	return (
 		<div className="flex items-center justify-star space-x-1 mt-3 mb-5">
-			{process.env.NEXT_PUBLIC_COLLECTION_TOKEN && (
 				<>
 					<a
 						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
@@ -51,7 +50,6 @@ const MarketPlaceLinks = ({ id }: { id: string }) => {
 						</div>
 					</a> */}
 				</>
-			)}
 		</div>
 	);
 };
